95cf28bcb6ade4ea11ad4690b1c36ed258b9d144
qigeminhzhihaonana <mdhubu1996@outlook.com>
2018-11-24 09:49:41 +0800

    t

--- src/views/task/NewTask.vue
+++ src/views/task/NewTask.vue
@@ -247,28 +247,69 @@ export default {
             dialogFormVisible: false,
             dialogFormVisibleTask: false,
         rules: {
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          itemNo: [
+            { required: true, message: '请', trigger: 'change, blur' }
           ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
+          itemName: [
+              {required: true,messagr: '', trigger: 'blur, change'}
           ],
-          date1: [
+          customerType:[
+              {required: true, message: '', trigger: 'blur, change'}
+          ],
+          businessType: [
+              {reequired: true, message: '', trigger: 'blur, change'}
+          ],
+          taskSource: [
+              {required: true, message: '', trigger: 'blur, change'}
+          ],
+          businessType: [
+              {
+                  required: true, 
+                  message: '',
+                  trigger: 'bulr, change'
+              }
+          ],
+          sampleCkType: [
+              {
+                  required:  true,
+                  message: '',
+                  trigger: 'blur, change'
+              }
+          ],
+          taskSource: [
+              {
+                  required: true,
+                  message: '',
+                  trigger: 'blur, change'
+              }
+          ],
+          managerUser: [
+              {
+                  required: true,
+                  message: '',
+                  trigger: 'blur, change'
+              }
+          ],
+          managerUser: [
+              {
+                  required: true,
+                  message: '',
+                  trigger: 'blur, change'
+              }
+          ],
+          planSum: [
+              {
+                  required: true,
+                  message : '',
+                  trigger: 'blur, change'
+              }
+          ],
+          buildTime: [
             { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
           ],
-          date2: [
+          planDate: [
             { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
           ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
-          ]
     },
     create() {
         this.onSubmit()
